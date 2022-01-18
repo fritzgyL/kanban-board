ac0049ab8f3f70e971ed23674de8ee3461ab5f62
Lubin Fritzgy <fritzgy.lubin@etudiant.univ-rennes1.fr>
2022-01-18 10:50:08 +0100

    feat: add enpoint for getting board by id

--- backend/src/main/java/rest/BoardResource.java
+++ backend/src/main/java/rest/BoardResource.java
@@ -1,26 +1,31 @@
 package rest;
 
-import java.util.List;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import repository.BoardRepository;
+import service.BoardService;
 import entity.Board;
 
-@Path("/board")
+@Path("/boards")
 @Produces({ "application/json", "application/xml" })
 public class BoardResource {
 
-	BoardRepository repository = new BoardRepository();
+	BoardService service = new BoardService();
 
 	@GET
-	@Path("/boards")
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<Board> getBoards() {
-		return repository.findAll();
+	public Iterable<Board> getBoards() {
+		return service.getBoards();
+	}
+
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Board getBoardById(@PathParam("id") long id) {
+		return service.getBoardById(id);
 	}
 
 }
--- backend/src/main/java/service/BoardService.java
+++ backend/src/main/java/service/BoardService.java
@@ -1,19 +1,18 @@
 package service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import entity.Board;
 import repository.BoardRepository;
 
-@Service
 public class BoardService {
 
-	@Autowired
-	private BoardRepository boardRepository;
+	private BoardRepository boardRepository = new BoardRepository();
 
 	public Iterable<Board> getBoards() {
 		return boardRepository.findAll();
+	}
+
+	public Board getBoardById(long id) {
+		return boardRepository.read(id);
 	}
 
 }
